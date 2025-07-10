b7651e96f562bc830b8d21ac8fc5ad9d5b0c60e9
Andrew <anding@student.ethz.ch>
2025-07-09 17:40:22 -0700

    refactor map [canton hook complete]

--- src/components/Map_refactor.jsx
+++ src/components/Map_refactor.jsx
@@ -1,75 +1,47 @@
-// HIGHLY WIP, VERY BROKEN ATM
-
-import React, { useMemo } from 'react';
+import React, { useRef } from 'react';
 import useMapbox from './map/useMapbox';
-import useCantons from './map/useCantons';
-import useNetworkLayers from './map/useNetworkLayers';
-import useTransitStops from './map/useTransitStops';
-import useChoropleth from './map/useChoropleth';
-import useMapPadding from './map/useMapPadding';
-import './Loading.css'; // spinner styles
+import useCantons             from './map/useCantons';
+import useNetworkLayers       from './map/useNetworkLayers';
+import useTransitStops        from './map/useTransitStops';
+import useChoropleth          from './map/useChoropleth';
+import useMapPadding          from './map/useMapPadding';
 
 export default function Map(props) {
-  const { mapRef, mapContainerRef } = useMapbox(import.meta.env.VITE_MAPBOX_TOKEN);
+  const { mapRef, mapContainerRef, mapReady } = useMapbox(import.meta.env.VITE_MAPBOX_TOKEN);
+
+  const suppressNextSearchZoom = useRef(false);
 
   useCantons({
     mapRef,
+    mapReady,
     dataURL: props.dataURL,
-    onCantonSelect: props.setClickedCanton,
+    setClickedCanton:     props.setClickedCanton,
+    searchCanton:         props.searchCanton,
+    isSidebarOpen:        props.isSidebarOpen,
+    isGraphExpanded:      props.isGraphExpanded,
+    suppressNextSearchZoom: suppressNextSearchZoom
   });
 
-  const activeCanton = useMemo(
-    () => props.searchCanton ?? props.clickedCanton ?? null,
-    [props.searchCanton, props.clickedCanton],
-  );
+  // useNetworkLayers({
+  //   mapRef,
+  //   canton:               props.clickedCanton,
+  //   dataURL:              props.dataURL,
+  //   selectedNetworkModes: props.selectedNetworkModes,
+  //   showMajorRoadsOnly:   props.showMajorRoadsOnly,
+  //   timeRange:            props.timeRange,
+  //   visualizeLinkId:      props.visualizeLinkId,
+  //   setSelectedNetworkFeature: props.setSelectedNetworkFeature,
+  //   isGraphExpanded:      props.isGraphExpanded
+  // });
 
-  const { isLoading: isLoadingNetwork } = useNetworkLayers({
-    mapRef,
-    canton: activeCanton,
-    dataURL: props.dataURL,
-    selectedNetworkModes: props.selectedNetworkModes,
-    visualizeLinkId: props.visualizeLinkId,
-    setSelectedNetworkFeature: props.setSelectedNetworkFeature,
-    isGraphExpanded: props.isGraphExpanded,
-  });
-
-  useTransitStops({
-    mapRef,
-    canton: activeCanton,
-    dataURL: props.dataURL,
-    selectedTransitModes: props.selectedTransitModes,
-    showStopVolumeSymbology: props.showStopVolumeSymbology,
-    setSelectedTransitStop: props.setSelectedTransitStop,
-    setHighlightedLineId: props.setHighlightedLineId,
-    highlightedLineId: props.highlightedLineId,
-    setHighlightedRouteIds: props.setHighlightedRouteIds,
-    hoveredRouteId: props.hoveredRouteId,
-    isGraphExpanded: props.isGraphExpanded,
-  });
-
-  useChoropleth({
-    mapRef,
-    dataURL: props.dataURL,
-    selectedMode: props.selectedMode,
-    selectedDataset: props.selectedDataset,
-    aggCol: props.aggCol,
-  });
-
-  useMapPadding({
-    mapRef,
-    isSidebarOpen: props.isSidebarOpen,
-    isGraphExpanded: props.isGraphExpanded,
-  });
+  // useTransitStops({ /* copy-paste your old transit args */ });
+  // useChoropleth({ /* copy-paste your choropleth args */ });
+  // useMapPadding({ mapRef, isSidebarOpen: props.isSidebarOpen, isGraphExpanded: props.isGraphExpanded });
 
   return (
     <>
-      {isLoadingNetwork && (
-        <div className="map-loading-overlay">
-          <div className="spinner" />
-          <div className="loading-text">Loading network...</div>
-        </div>
-      )}
-      <div ref={mapContainerRef} style={{ width: '100%', height: '100%' }} />
+      {props.isLoadingNetwork && <div className="map-loading-overlay">…</div>}
+      <div ref={mapContainerRef} style={{ width:'100%',height:'100%' }}/>
     </>
   );
 }
